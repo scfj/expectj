e455d3b4f90d88939bf39e56221f5fcf2559104e
Dmitry Barskov <barskoidim@gmail.com>
2020-03-08 21:13:16 +0300

    Add test

--- src/test/java/com/github/scfj/expectj/ExpectationTest.java
+++ src/test/java/com/github/scfj/expectj/ExpectationTest.java
@@ -1,4 +1,41 @@
-package com.github.scfj.expectj.checks;
+package com.github.scfj.expectj;
 
-public class ExcpectationTest {
+import org.junit.Test;
+
+public class ExpectationTest {
+    @Test
+    public void should_check() {
+        Any<Long> beAny = new Any<Long>();
+        new Expectation<Long>(3L).to(beAny);
+        assert beAny.invokedWith(3L) : "Expected to invoke check with 3";
+    }
+
+    @Test(expected = AssertionError.class)
+    public void should_throw_assertion_error() {
+        None<Long> beNone = new None<Long>();
+        new Expectation<Long>(3L).to(beNone);
+    }
+
+    static class Any<T> implements Check<T> {
+        boolean invoked;
+        T value;
+
+        @Override
+        public boolean satisfies(T actual) {
+            invoked = true;
+            value = actual;
+            return true;
+        }
+
+        public boolean invokedWith(T value) {
+            return invoked && this.value.equals(value);
+        }
+    }
+
+    static class None<T> implements Check<T> {
+        @Override
+        public boolean satisfies(T actual) {
+            return false;
+        }
+    }
 }
